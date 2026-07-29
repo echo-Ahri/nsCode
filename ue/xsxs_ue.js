bbdd73d22de364063ed0dcda86cc80f371ebbb9c
不要放香菜 <122475408@qq.com>
2026-07-29 15:47:56 +0800

    自定义按钮增加类型判断

--- ue/xsxs_ue.js
+++ ue/xsxs_ue.js
@@ -8,7 +8,7 @@ define(['N/file', 'N/log', 'N/runtime', 'N/url', 'N/record', 'N/search', 'N/conf
         var currentUser = runtime.getCurrentUser(); //获取登录用户的角色
 
         var form = context.form;
-        form.removeButton('convertlead'); //删除转换按钮
+        // form.removeButton('convertlead'); //删除转换按钮
 
         var rec = context.newRecord; //当前打开的记录
         var rec_id = rec.id; //当前记录的id
@@ -19,11 +19,14 @@ define(['N/file', 'N/log', 'N/runtime', 'N/url', 'N/record', 'N/search', 'N/conf
             id: 'SuiteScripts/dsp_scripts/cs/xsxs_cs.js',
         });
         form.clientScriptFileId = fileObj.id;
+        
+        var type = rec.getValue('isperson');
+        // log.debug('type', {'type':type });
 
-        if (!isEmpty(rec_id)) {
+        if (!isEmpty(rec_id) && type) {  //是 个人 类型才能转换
             form.addButton({
                 id: 'custpage_xszh_btn',
-                label: '[线索转换客户]',
+                label: '[销售线索转换潜在客户]',
                 functionName: 'xsZhKh(' + rec_id + ',"' + rec_type + '")',
             });
         }
